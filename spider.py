d7c834fb06cf06fcadab50435b9c816f52fd9d37
Yan Ren <ryan.ren3@gmail.com>
2019-01-14 21:13:20 -0500

    working on spider

--- spider.py
+++ spider.py
@@ -12,5 +12,28 @@ class Spider:
     queue = set()
     crawled = set()
 
-    def __init__(self):
-        pass
+    def __init__(self, project_name, base_url, domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + "/queue.txt"
+        Spider.crawled_file = Spider.project_name + "/crawled.txt"
+        self.boot()
+        self.crawl_page('Homepage', Spider.base_url)
+
+    @staticmethod
+    def boot():
+        create_project_dir(Spider.project_name)
+        create_data_files(Spider.project_name, Spider.base_url)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set(Spider.crawled_file)
+
+
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        print(thread_name + ' now crawling ' + page_url)
+        print('Queue ' + str(len(Spider.queue)) + ' | Crawled ' + str(len(Spider.crawled)))
+        Spider.add_links_to_queue(Spider.gather_link(page_url))
+        Spider.queue.remove(page_url)
+        Spider.crawled.add(page_url)
+        Spider.update_files()
