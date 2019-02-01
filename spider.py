9b676e0b4c27ffe423ac8cdf53d754a7e6ecec3e
Yan Ren <yan.ren@indexexchange.com>
2019-02-01 15:11:10 -0500

    finish up, able to run and crawl pages

--- spider.py
+++ spider.py
@@ -32,7 +32,7 @@ class Spider:
     def crawl_page(thread_name, page_url):
         print(thread_name + ' now crawling ' + page_url)
         print('Queue ' + str(len(Spider.queue)) + ' | Crawled ' + str(len(Spider.crawled)))
-        Spider.add_links_to_queue(Spider.gather_link(page_url))
+        Spider.add_links_to_queue(Spider.gather_links(page_url))
         Spider.queue.remove(page_url)
         Spider.crawled.add(page_url)
         Spider.update_files()
@@ -42,7 +42,7 @@ class Spider:
         html_string = ''
         try:
             response = urlopen(page_url)
-            if response.getheader('Content-Type') == 'text/html':
+            if 'text/html' in response.getheader('Content-Type'):
                 html_bytes = response.read()
                 html_string = html_bytes.decode("utf-8")
             finder = LinkFinder(Spider.base_url, page_url)
